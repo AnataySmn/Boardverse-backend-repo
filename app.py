f9087b214eadbece4b3bb8883005b89ee7c94234
VaTruShka01 <200521445@student.georgianc.on.ca>
2024-11-30 22:44:29 -0500

    fixed CORS

--- app.py
+++ app.py
@@ -4,25 +4,27 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from extensions import db
 from routes.users import user_blueprint
+from flask_cors import CORS
 
 # Load environment variables
 load_dotenv()
 
 app = Flask(__name__)
 
+
+CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE"]}})
+
+
 # Configure the database
 db_uri = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Initialize extensions
 db.init_app(app)
 bcrypt = Bcrypt(app)
 
-# Register blueprints
 app.register_blueprint(user_blueprint, url_prefix='/user')
 
-# Create database tables (for development)
 with app.app_context():
     db.create_all()
 
