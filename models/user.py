26b6800284c690b5a2b3671fa509a4edb8f85dc8
VaTruShka01 <200521445@student.georgianc.on.ca>
2025-02-23 19:14:14 -0500

    Changed default status from away to online

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ class User(db.Model):
     userName = db.Column(db.String(255), nullable=False)
     userEmail = db.Column(db.String(255), unique=True, nullable=False)
     userPassword = db.Column(db.String(255), nullable=False)
-    userStatus = db.Column(db.String(50), default='Away', nullable=False)
+    userStatus = db.Column(db.String(50), default='Online', nullable=False)
     userProfileDescription = db.Column(db.Text, nullable=True)
     userProfilePic = db.Column(db.LargeBinary, nullable=True)
     userLevel = db.Column(db.Integer, default=1, nullable=False)
